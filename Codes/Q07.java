87badb0b434f347bdd0a716d56495660fb03d83b
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2022-12-11 21:47:16 +0900

    Git教材執筆開始

---
+++ Codes/Q07.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Q07 {
+
+  public static void main(String[] args) {
+    double val = 60.4;
+    int i;
+    i = (int)val + 20 / 2;
+    System.out.println(i);
+    int r = Random.nextInt(10);
+    System.out.println(r);
+  }
+}
